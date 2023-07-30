f132d862822d7bb68f2f47e700fcb2104c6a89b2
samjay <sanjay7794sy@gmail.com>
2023-07-30 17:01:29 +0530

    cart undisplay when adding itom in cart

--- Eshop/store/Views/home.py
+++ Eshop/store/Views/home.py
@@ -8,9 +8,25 @@ from  django.views import View
 
 class Index(View):
     def post(self, request):
-        product_id=request.POST.get('product')
-        print(product_id)
+        product=request.POST.get('product')
+        cart=request.session.get('cart')
+        if cart:
+            quantity=cart.get(product)
+            if quantity:
+                cart[product]=quantity+1  
+            else:
+                cart[product]=1            
+        else:
+            cart={}
+            cart[product]=1
+        request.session['cart']=cart  
+        print('cart  :',request.session['cart'])     
         return redirect('homepage')
+
+
+
+
+
 
     def get(self, request):
         prds = None
