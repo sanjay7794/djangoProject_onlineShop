d6231c8cab791a1f7737b8bb77a21100807b95ea
samjay <sanjay7794sy@gmail.com>
2023-08-11 10:07:55 +0530

    orders .py file created

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models.product import *
 from .models.category import *
 from .models.customer import Customer
+from .models.orders import Orders
 
 class AdminProduct(admin.ModelAdmin):
     list_display=['name','price','category']
@@ -15,3 +16,4 @@ admin.site.register(Product,AdminProduct)
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Customer)
+admin.site.register(Orders)
---
+++ store/models/orders.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .product import Product
+from .customer import Customer
+import datetime
+
+
+class Orders(models.Model):
+    product = models.ForeignKey( Product, on_delete=models.CASCADE)    
+    customer = models.ForeignKey( Customer, on_delete=models.CASCADE)  
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address=models.CharField(max_length=100,blank=True)
+    phone=models.CharField(max_length=10,blank=True)
+    date=models.DateField(default=datetime.datetime.today)
